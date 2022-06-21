d08ea583cb53ada9858e6f9dba8a658722a58584
Mariana Cevidanes Lopes <mariana.lopes@gpnet.com.br>
2022-06-21 15:09:57 -0300

    Adicionando repositories

---
+++ biblioteca/src/main/java/br/com/aceleragep/biblioteca/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package br.com.aceleragep.biblioteca.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.aceleragep.biblioteca.entities.LivroEntity;
+
+@Repository
+public interface LivroRepository extends JpaRepository<LivroEntity, Long> {
+
+}
