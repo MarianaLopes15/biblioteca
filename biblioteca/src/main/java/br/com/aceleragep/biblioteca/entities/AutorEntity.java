7c7724041493bc530023164c04aea012e4458368
Mariana Cevidanes Lopes <mariana.lopes@gpnet.com.br>
2022-06-21 15:01:20 -0300

    Adicionando entities e configs

---
+++ biblioteca/src/main/java/br/com/aceleragep/biblioteca/entities/AutorEntity.java
@@ -0,0 +1,29 @@
+package br.com.aceleragep.biblioteca.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_autores")
+public class AutorEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "nome", length = 100)
+	private String nome;
+
+	@Column(name = "bibliografia", length = 100)
+	private String bibliografia;
+}
