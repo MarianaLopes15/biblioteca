f75d00b126b718145ca4fe4d38588496bab6f090
Mariana Cevidanes Lopes <mariana.lopes@gpnet.com.br>
2022-07-08 11:15:23 -0300

    Refatorando codigo

--- biblioteca/src/main/java/br/com/aceleragep/biblioteca/converts/LivroConvert.java
+++ biblioteca/src/main/java/br/com/aceleragep/biblioteca/converts/LivroConvert.java
@@ -1,14 +1,10 @@
 package br.com.aceleragep.biblioteca.converts;
 
-import java.util.stream.Collectors;
-
 import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import br.com.aceleragep.biblioteca.dtos.inputs.LivroInput;
@@ -24,7 +20,6 @@ public class LivroConvert {
 
 	public LivroEntity inputToNewEntity(@Valid LivroInput livroInput) {
 		LivroEntity livroEntity = modelMapper.map(livroInput, LivroEntity.class);
-//		converteIdsAutorParaAutores(livroInput, livroEntity);
 		return livroEntity;
 	}
 
@@ -38,32 +33,10 @@ public class LivroConvert {
 
 	public void copyInputToEntity(LivroEntity livroLocalizado, @Valid LivroInput livroInput) {
 		modelMapper.map(livroInput, livroLocalizado);
-//		converteIdsAutorParaAutores(livroInput, livroLocalizado);
-
 	}
-
-	public Page<LivroOutput> entityToOutput(Page<LivroEntity> livrosLocalizados, Pageable paginacao) {
-		return new PageImpl<>(livrosLocalizados.stream().map(livroEntity -> {
-			return entityToOutput(livroEntity);
-		}).collect(Collectors.toList()));
-	}
-
-	/*
-	 * private void converteIdsAutorParaAutores(LivroInput livroInput, LivroEntity
-	 * livroEntity) { List<AutorEntity> autores = new ArrayList<>(); for (Long
-	 * idAutor : livroInput.getIdsAutores()) { AutorEntity autor =
-	 * autorService.buscaPeloId(idAutor); autores.add(autor); }
-	 * livroEntity.setAutores(autores); }
-	 */
 
 	public LivroAutorOutput entityToSemAutorOutput(LivroEntity livroEntity) {
 		return modelMapper.map(livroEntity, LivroAutorOutput.class);
-	}
-
-	public Page<LivroAutorOutput> entityToSemAutorOutput(Page<LivroEntity> livros, Pageable paginacao) {
-		return new PageImpl<>(livros.stream().map(livroEntity -> {
-			return entityToSemAutorOutput(livroEntity);
-		}).collect(Collectors.toList()));
 	}
 
 	public Page<LivroAutorOutput> listPageEntityToListPageOutputCopy(Page<LivroEntity> livros) {
